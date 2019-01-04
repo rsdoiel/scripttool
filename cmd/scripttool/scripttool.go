fa3fd23fac8ca94be072a17e9285662ca6bfb4fb
R. S. Doiel <rsdoiel@gmail.com>
2019-01-04 13:41:15 -0800

    Quick Save

--- cmd/scripttool/scripttool.go
+++ cmd/scripttool/scripttool.go
@@ -29,9 +29,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 
 	// Caltech Library packages
@@ -132,30 +132,65 @@ func onError(eout io.Writer, err error) int {
 	return 0
 }
 
-func doFdxToFountain(in io.Reader, out io.Writer, eout io.Writer, args []string) int {
+func doFdxToFountain(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
 	return onError(eout, scripttool.FdxToFountain(in, out))
 }
 
-func doFountainToFdx(in io.Reader, out io.Writer, eout io.Writer, args []string) int {
+func doFountainToFdx(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
 	return onError(eout, scripttool.FountainToFdx(in, out))
 }
 
-func doOSFToFountain(in io.Reader, out io.Writer, eout io.Writer, args []string) int {
+func doOSFToFountain(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
 	return onError(eout, scripttool.OSFToFountain(in, out))
 }
 
-func doFountainToOSF(in io.Reader, out io.Writer, eout io.Writer, args []string) int {
+func doFountainToOSF(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
 	return onError(eout, scripttool.FountainToOSF(in, out))
 }
 
-func doCharacters(in io.Reader, out io.Writer, eout io.Writer, args []string) int {
+func doCharacters(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
 	return onError(eout, scripttool.CharacterList(in, out))
+}
+
+func doFadeInToFountain(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
+	if inputFName == "" && len(args) > 0 {
+		inputFName = args[0]
+	}
+	if inputFName == "" {
+		fmt.Fprintf(eout, "Missing input FadeIn filename\n")
+		return 1
+	}
+	return onError(eout, scripttool.FadeInToFountain(inputFName, out))
+}
+
+func doFountainToFadeIn(in io.Reader, out io.Writer, eout io.Writer, args []string, flagSet *flag.FlagSet) int {
+	var err error
+
+	if inputFName == "" && len(args) > 0 {
+		inputFName = args[0]
+		in, err = cli.Open(inputFName, os.Stdin)
+		cli.ExitOnError(os.Stderr, err, quiet)
+	}
+	if outputFName == "" && len(args) > 1 {
+		outputFName = args[1]
+	}
+	if inputFName == "" {
+		fmt.Fprintf(eout, "Missing input fountain filename\n")
+		return 1
+	}
+	if outputFName == "" {
+		fmt.Fprintf(eout, "Missing output FadeIn filename\n")
+		return 1
+	}
+	return onError(eout, scripttool.FountainToFadeIn(in, outputFName))
 }
 
 func main() {
 	app := cli.NewCli(scripttool.Version)
 	appName := app.AppName()
-	app.AddParams("ACTION", "[ACTION PARAMETERS]")
+	app.SetParams("VERB", "[VERB PARAMETERS]")
+
+	// Standard flags
 	app.AddHelp("synopsis", []byte(synopsis))
 	app.AddHelp("description", []byte(description))
 	app.AddHelp("examples", []byte(examples))
@@ -171,14 +206,28 @@ func main() {
 	app.StringVar(&outputFName, "o,output", "", "set output filename")
 	app.BoolVar(&newLine, "nl,newline", true, "add a trailing newline to output")
 
-	app.AddAction("fdx2fountain", doFdxToFountain, "convert .fdx to .fountain")
-	app.AddAction("osf2fountain", doOSFToFountain, "convert .osf to .fountain")
-	app.AddAction("fountain2fdx", doFountainToFdx, "convert .fountain to .fdx")
-	app.AddAction("fountain2osf", doFountainToOSF, "convert .fountain to .osf")
-	app.AddAction("characters", doCharacters, "list characters in a screenplay")
+	// Add Verbs
+	app.VerbsRequired = true
+	vFdxToFountain := app.NewVerb("fdx2fountain", "convert .fdx to .fountain", doFdxToFountain)
+	vFdxToFountain.SetParams("[INPUT_FILENAME]", "[OUTPUT_FILENAME]")
 
-	app.AddVerb("fadein2fountain", "convert .fadein to .fountain")
-	app.AddVerb("fountain2fadein", "convert .fountain to .fadein")
+	vOSFToFountain := app.NewVerb("osf2fountain", "convert .osf to .fountain", doOSFToFountain)
+	vOSFToFountain.SetParams("[INPUT_FILENAME]", "[OUTPUT_FILENAME]")
+
+	vFountainToFdx := app.NewVerb("fountain2fdx", "convert .fountain to .fdx", doFountainToFdx)
+	vFountainToFdx.SetParams("[INPUT_FILENAME]", "[OUTPUT_FILENAME]")
+
+	vFountainToOSF := app.NewVerb("fountain2osf", "convert .fountain to .osf", doFountainToOSF)
+	vFountainToOSF.SetParams("[INPUT_FILENAME]", "[OUTPUT_FILENAME]")
+
+	vCharacters := app.NewVerb("characters", "list characters in a screenplay", doCharacters)
+	vCharacters.SetParams("[INPUT_FILENAME]")
+
+	vFadeInToFountain := app.NewVerb("fadein2fountain", "convert .fadein to .fountain", doFadeInToFountain)
+	vFadeInToFountain.SetParams("INPUT_FILENAME", "[OUTPUT_FILENAME]")
+
+	vFountainToFadeIn := app.NewVerb("fountain2fadein", "convert .fountain to .fadein", doFountainToFadeIn)
+	vFountainToFadeIn.SetParams("INPUT_FILENAME", "OUTPUT_FILENAME")
 
 	// Parse environment and command line
 	if err := app.Parse(); err != nil {
@@ -212,123 +261,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Map verb phrases to standard options
-	var (
-		verb             string
-		fadeInToFountain bool
-		fountainToFadeIn bool
-	)
-
-	switch app.Verb(args) {
-	case "fdx2fountain":
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-	case "fountain2fdx":
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-	case "fadein2fountain":
-		fadeInToFountain = true
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if inputFName == "" {
-			log.Fatal("Missing FadeIn input filename")
-		}
-	case "fountain2fadein":
-		fountainToFadeIn = true
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" {
-			log.Fatal("Missing FadeIn output filename")
-		}
-	case "osf2fountain":
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-	case "fountain2osf":
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-		if outputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			outputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-	case "characters":
-		if inputFName == "" && len(args) > 1 {
-			verb, args = cli.ShiftArg(args)
-			inputFName, args = cli.ShiftArg(args)
-			args = cli.UnshiftArg(verb, args)
-		}
-	}
-
 	// Setup IO
 	var err error
 
 	app.Eout = os.Stderr
 
-	if fadeInToFountain {
-		app.Out, err = cli.Create(outputFName, os.Stdout)
-		cli.ExitOnError(app.Eout, err, quiet)
-		err = scripttool.FadeInToFountain(inputFName, app.Out)
-		cli.ExitOnError(app.Eout, err, quiet)
-		if newLine {
-			fmt.Fprintln(app.Out, "")
-		}
-		os.Exit(0)
-	}
-	if fountainToFadeIn {
-		if inputFName != "" {
-			log.Printf("Reading %s", inputFName)
-		}
-		app.In, err = cli.Open(inputFName, os.Stdin)
-		cli.ExitOnError(app.Eout, err, quiet)
-		log.Printf("Writing %s", outputFName)
-		err = scripttool.FountainToFadeIn(app.In, outputFName)
-		log.Printf("DEBUG Error: %s", err)
-		cli.ExitOnError(app.Eout, err, quiet)
-		os.Exit(0)
-	}
+	app.In, err = cli.Open(inputFName, os.Stdin)
+	cli.ExitOnError(app.Eout, err, quiet)
+	defer cli.CloseFile(inputFName, app.In)
+
+	app.Out, err = cli.Create(outputFName, os.Stdout)
+	cli.ExitOnError(app.Eout, err, quiet)
+	defer cli.CloseFile(outputFName, app.Out)
 
 	app.In, err = cli.Open(inputFName, os.Stdin)
 	cli.ExitOnError(app.Eout, err, quiet)
