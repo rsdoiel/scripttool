70c880b18f1f209529e2431fa1c5c7fd1c2c316d
R. S. Doiel <rsdoiel@gmail>
2024-05-20 14:38:45 -0700

    Quick Save

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2024-05-20"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "28900db"
+    ReleaseHash = "97d9684"
 
     LicenseText = `
 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
